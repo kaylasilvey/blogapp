5f9dda9a0595e52b73b2b11786723605dc9581a3
Kayla Silvey <ksilvey19@gmail.com>
2019-08-13 17:28:25 -0500

    practice app for testing

---
+++ src/views/Post.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="home">
+    <h1>{{ message }}</h1>
+    <div v-for="post in posts"></div>
+    <h1>{{ post.title }}</h1>
+    <p>{{ post.body }}</p>
+    <img src="" />
+  </div>
+</template>
+
+<style></style>
+
+<script>
+import axios from "axios";
+export default {
+  data: function() {
+    return {
+      message: "Welcome to BLOG!",
+      posts: [],
+      title: "",
+      body: ""
+    };
+  },
+  created: function() {
+    axios.get("/api/posts").then(response => {
+      this.posts = response.data;
+      console.log(this.posts);
+    });
+  },
+  methods: {}
+};
+</script>
